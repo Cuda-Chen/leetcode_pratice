e8faed79fb7c3320a089719c5bd350a66a5fc96e
Cuda-Chen <clh960524@gmail.com>
2019-07-23 00:37:43 +0800

    problem 730: add CUDA multithread feature

--- 730/730_solution.cu
+++ 730/730_solution.cu
@@ -16,36 +16,35 @@ inline void checkError(cudaError_t status) {
     }
 }
 
-__global__ void helperKernel(char *S, int *dp, int n, long kMod) {
-   for(int len = (blockIdx.x * blockDim.x + threadIdx.x) + 1; len <= n; len += blockDim.x * gridDim.x) {
-       for(int i = blockIdx.y * blockDim.y + threadIdx.y; i < n - len; i += blockDim.y * gridDim.y) {
-           int j = i + len; // jth element is the end of current string
-           if(S[i] == S[j]) { // if front and rear are the same
-               dp[i * n + j] = dp[(i + 1) * n + (j - 1)] * 2;
-               int left = i + 1;
-               int right = j - 1;
-                
-               while(left <= right && S[left] != S[i]) {
-                   left++;
-               }
-               while(left <= right && S[right] != S[i]) {
-                   right--;
-               }
-                
-               if(left == right) {
-                   dp[(i * n) + j] += 1;
-               } else if(left > right) {
-                   dp[(i * n) + j] += 2;
-               } else {
-                   dp[(i * n) + j] -= dp[(left + 1) * n + (right - 1)];
-               }
-            } else {
-                dp[(i * n) + j] = dp[(i * n) + (j - 1)] + dp[((i + 1) * n) + j] - dp[((i + 1) * n) + (j - 1)];
-            }
-            
-            dp[(i * n) + j] = (dp[(i * n) + j] + kMod) % kMod; // perform positive modulo
-        }
-    }
+__global__ void helperKernel(char *S, int *dp, int n, long kMod, int len) {
+  for(int i = blockIdx.x * blockDim.x + threadIdx.x; i < n - len; i += blockDim.x * gridDim.x) {
+      int j = i + len; // jth element is the end of current string
+      if(S[i] == S[j]) { // if front and rear are the same
+          dp[i * n + j] = dp[(i + 1) * n + (j - 1)] * 2;
+          int left = i + 1;
+          int right = j - 1;
+           
+          while(left <= right && S[left] != S[i]) {
+              left++;
+          }
+          while(left <= right && S[right] != S[i]) {
+              right--;
+          }
+           
+          if(left == right) {
+              dp[(i * n) + j] += 1;
+          } else if(left > right) {
+              dp[(i * n) + j] += 2;
+          } else {
+              dp[(i * n) + j] -= dp[(left + 1) * n + (right - 1)];
+          }
+       } else {
+           dp[(i * n) + j] = dp[(i * n) + (j - 1)] + dp[((i + 1) * n) + j] - dp[((i + 1) * n) + (j - 1)];
+       }
+       
+       dp[(i * n) + j] = (dp[(i * n) + j] + kMod) % kMod; // perform positive modulo
+   }
+   __syncthreads();
 }
 
 int countPalindromicSubsequences(char * S){
@@ -115,7 +114,9 @@ int countPalindromicSubsequences(char * S){
     checkError(cudaMemcpy(d_dp, h_dp, n * n * sizeof(int), cudaMemcpyHostToDevice));
     checkError(cudaMemcpy(d_S, S, n * sizeof(char), cudaMemcpyHostToDevice));
 
-    helperKernel<<<1, 1>>>(d_S, d_dp, n, kMod);
+    for(int len = 1; len <= n; len++) {
+        helperKernel<<<1, 1024>>>(d_S, d_dp, n, kMod, len);
+    }
 
     checkError(cudaMemcpy(h_dp, d_dp, n * n * sizeof(int), cudaMemcpyDeviceToHost));
 
@@ -125,7 +126,7 @@ int countPalindromicSubsequences(char * S){
             break;
         }
     }
-    printf("value of dp[0][n - 1]: %d\n", h_dp[(0 * n) + (n - 1)]);
+    printf("value of h_dp[0][n - 1]: %d\n", h_dp[(0 * n) + (n - 1)]);
 
     cudaFree(d_dp);
     cudaFree(d_S);
@@ -138,11 +139,11 @@ int countPalindromicSubsequences(char * S){
 int main() {
     //char *input = "bccb";
     char *input = "abcdabcdabcdabcdabcdabcdabcdabcddcbadcbadcbadcbadcbadcbadcbadcba";
-    clock_t t;
-    t = clock();
+    //clock_t t;
+    //t = clock();
     int result = countPalindromicSubsequences(input);
-    t = clock() - t;
-    printf("execution time: %f\n", ((float)t / CLOCKS_PER_SEC));
+    //t = clock() - t;
+    //printf("execution time: %f\n", ((float)t / CLOCKS_PER_SEC));
     printf("Output: %d\n", result);
     return 0;
 }
