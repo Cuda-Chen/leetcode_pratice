28adaa960dedb42925cb79983263f7ab7e789417
Cuda-Chen <clh960524@gmail.com>
2019-10-19 22:11:14 +0800

    2: make some changes of Ruby solution
    Make some changes of Ruby solution according to Tatsujin's
    advice in problem #2.

--- 2/main.rb
+++ 2/main.rb
@@ -11,24 +11,27 @@
 # # @param {ListNode} l2
 # # @return {ListNode}
 def add_two_numbers(l1, l2)
-    dummyHead = ListNode.new(0)
-    current = dummyHead
+    current = dummyHead = ListNode.new(0)
     carry = 0
     
-    while l1 or l2
+    while l1 || l2
         val1 = l1 ? l1.val : 0
         val2 = l2 ? l2.val : 0
-        sum = carry + val1 + val2
+        sum = carry
+        if l1 
+            sum += l1.val
+            l1 = l1.next
+        end
+        if l2
+            sum += l2.val
+            l2 = l2.next
+        end
         carry = sum / 10
         current.next = ListNode.new(sum % 10)
         current = current.next
-        l1 = l1 ? l1.next : nil
-        l2 = l2 ? l2.next : nil
     end
     
-    if carry > 0
-        current.next = ListNode.new(carry)
-    end
+    current.next = ListNode.new(carry) if carry > 0
     
     return dummyHead.next
 end
