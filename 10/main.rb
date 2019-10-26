362953d17ac3f83d169e0434507db750c6f02c8b
Cuda-Chen <clh960524@gmail.com>
2019-10-26 23:02:57 +0800

    10: remove all ```return``` keyword
    Remove all ```return``` keyword by Tatsujin's advice.

--- 10/main.rb
+++ 10/main.rb
@@ -3,16 +3,14 @@
 # @param {String} p
 # @return {Boolean}
 def is_match(s, p)
-  if p.empty?
-      return s.empty?
-  end
+      return s.empty? if p.empty?
     
   first_match = !s.empty? && (p[0] == s[0] || p[0] == '.')
     
   if p.size >= 2 && p[1] == '*'
-      return is_match(s, p[2...p.size]) ||
+      is_match(s, p[2...p.size]) ||
              first_match && is_match(s[1...s.size], p)
   else
-      return first_match && is_match(s[1...s.size], p[1...p.size])
+      first_match && is_match(s[1...s.size], p[1...p.size])
   end
 end
