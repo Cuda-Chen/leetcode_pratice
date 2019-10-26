b513bc96006bc174850b9f9c8b2d554aec30f75c
Cuda-Chen <clh960524@gmail.com>
2019-10-26 22:44:28 +0800

    10: change some lines of Ruby recursive solution
    Change some lines of Ruby recursive solution on Tatsujin's advice.

--- 10/main.rb
+++ 10/main.rb
@@ -3,16 +3,16 @@
 # @param {String} p
 # @return {Boolean}
 def is_match(s, p)
-  if p.empty?()
-      return s.empty?()
+  if p.empty?
+      return s.empty?
   end
-  
-  first_match = (!s.empty?() && (p[0] == s[0] || p[0] == '.'))
-  
-  if p.length >= 2 && p[1] == '*'
-      return (is_match(s, p[2..p.length - 1])) ||
-             (first_match && is_match(s[1..s.length - 1], p))
+    
+  first_match = !s.empty? && (p[0] == s[0] || p[0] == '.')
+    
+  if p.size >= 2 && p[1] == '*'
+      return is_match(s, p[2...p.size]) ||
+             first_match && is_match(s[1...s.size], p)
   else
-      return first_match && is_match(s[1..s.length - 1], p[1..p.length - 1])
+      return first_match && is_match(s[1...s.size], p[1...p.size])
   end
 end
