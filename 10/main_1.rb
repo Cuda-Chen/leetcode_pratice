fd0d1917f4f2e921ddd84a5b4fba2b2dfe228596
Cuda-Chen <clh960524@gmail.com>
2019-10-26 22:33:13 +0800

    10: change indent and remove return keyword in Ruby iterative solution
    Change indent of if...else statement and remove return keyword
    in Ruby iterative solution by Tatsujin's advice.

--- 10/main_1.rb
+++ 10/main_1.rb
@@ -17,12 +17,12 @@ def is_match(s, p)
    (1...p_len).each do |i|
        (1...s_len).each do |j|
            dp[i][j] = if p[i - 1] == '*' 
-               dp[i - 2][j] || ((dp[i - 2][j - 1] || dp[i][j - 1]) && (p[i - 2] == '.' || p[i - 2] == s[j - 1]))
-           else
-               dp[i - 1][j - 1] && (p[i - 1] == '.' || p[i - 1] == s[j - 1])
-           end
+                        dp[i - 2][j] || ((dp[i - 2][j - 1] || dp[i][j - 1]) && (p[i - 2] == '.' || p[i - 2] == s[j - 1]))
+                      else
+                        dp[i - 1][j - 1] && (p[i - 1] == '.' || p[i - 1] == s[j - 1])
+                      end
        end
    end
     
-  return dp[p.size][s.size]
+  dp[p.size][s.size]
 end
