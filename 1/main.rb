b199cdcd3109097a3c958de263f6525f4e9d30a5
Cuda-Chen <clh960524@gmail.com>
2019-11-04 21:11:04 +0800

    1: update Ruby solution again
    Update Ruby solution again according to Tatsujin's advice.

--- 1/main.rb
+++ 1/main.rb
@@ -4,17 +4,12 @@
 # @param {Integer} target
 # @return {Integer[]}
 def two_sum(nums, target)
-    n = nums.size
-    result = Array.new
     hash_table = {}
     
     nums.each_with_index do |n, i|
         complement = target - nums[i]
         return hash_table[complement], i if hash_table.has_key? complement
-            
+
         hash_table[nums[i]] = i
-        end
     end
-    
-    result
 end
