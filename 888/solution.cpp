bdd0168df2fee9e80e3313da8717ba28553668d2
Cuda Chen <clh960524@gmail.com>
2020-07-18 16:58:56 +0800

    add C++ solution of problem #888

---
+++ 888/solution.cpp
@@ -0,0 +1,20 @@
+// two sum variant
+
+class Solution {
+public:
+    vector<int> fairCandySwap(vector<int>& A, vector<int>& B) {
+        int sum1 = accumulate(A.begin(), A.end(), 0, plus<int>());
+        int sum2 = accumulate(B.begin(), B.end(), 0, plus<int>());
+        int diff = (sum1 - sum2) / 2;
+        unordered_set<int> s(A.begin(), A.end());
+        
+        for(auto x: B) {
+            int target = x + diff;
+            if(s.count(target)) {
+                return {target, x};
+            }
+        }
+        
+        return {};
+    }
+};
