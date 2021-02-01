69183736ecfc5baf1f18420279e3fcd7b9a48fcd
Cuda-Chen <clh960524@gmail.com>
2021-02-01 16:09:08 +0800

    add C++ solution of problem #191

---
+++ 191/solution.cpp
@@ -0,0 +1,12 @@
+// math
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int ans = 0;
+        while(n) {
+            if(n % 2 == 1) ans++;
+            n /= 2;
+        }
+        return ans;
+    }
+};
