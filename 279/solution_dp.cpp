646427459eb0542f50e7cf48cfe04bf01ae17618
Cuda-Chen <clh960524@gmail.com>
2021-04-21 14:37:55 +0800

    add C++ DP solution of problem #279

---
+++ 279/solution_dp.cpp
@@ -0,0 +1,17 @@
+// https://zxi.mytechroad.com/blog/dynamic-programming/leetcode-279-perfect-squares/
+// DP
+
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> dp(n + 1, INT_MAX >> 1);
+        dp[0] = 0;
+        
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j * j <= i; j++)
+                dp[i] = min(dp[i], dp[i - j * j] + 1);
+        }
+        
+        return dp[n];
+    }
+};
