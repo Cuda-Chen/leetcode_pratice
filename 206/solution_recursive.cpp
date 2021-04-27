0974e42c1be26d8dcbb7baab2dc44dbda876048e
Cuda Chen <clh960524@gmail.com>
2021-04-27 20:50:02 +0800

    add two C++ solutions of problem #206

---
+++ 206/solution_recursive.cpp
@@ -0,0 +1,25 @@
+// reverse
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head) return nullptr;
+        if(!head->next) return head;
+        
+        ListNode *last = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        
+        return last;
+    }
+};
