6e05b3e3416ca4414790025784e9afff2e168676
Cuda-Chen <clh960524@gmail.com>
2021-05-05 14:07:39 +0800

    add C++ DP solution of problem #343

---
+++ 343/solution_dp.cpp
@@ -0,0 +1,16 @@
+// https://www.cnblogs.com/grandyang/p/5411919.html
+// DP
+
+class Solution {
+public:
+    int integerBreak(int n) {
+        vector<int> dp(n + 1, 1);
+        
+        for(int i = 3; i <= n; i++) {
+            for(int j = 1; j < i; j++)
+                dp[i] = max(dp[i], max(j * (i - j), j * dp[i - j]));
+        }
+        
+        return dp[n];
+    }
+};
