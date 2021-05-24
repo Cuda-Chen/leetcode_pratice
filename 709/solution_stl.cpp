4e493741bac1006af696dd0d9dfd7dd921611984
Cuda Chen <clh960524@gmail.com>
2021-05-24 15:40:37 +0800

    add C++ STL solution of problem #709

---
+++ 709/solution_stl.cpp
@@ -0,0 +1,11 @@
+// C++ STL
+class Solution {
+public:
+    string toLowerCase(string s) {
+        string result;
+        for(char c : s) {
+            result += tolower(c);
+        }
+        return result;
+    }
+};
