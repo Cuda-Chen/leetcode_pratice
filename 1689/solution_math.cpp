c3749fd57addd57b1c16dd2b2e7bf3ad3fc74c12
Cuda-Chen <clh960524@gmail.com>
2021-05-26 15:26:30 +0800

    add C++ math solution of problem #1689

---
+++ 1689/solution_math.cpp
@@ -0,0 +1,12 @@
+// math
+// for each digit, the value will equal to binary digit minimum sum.
+class Solution {
+public:
+    int minPartitions(string n) {
+        int result = 0;
+        for(char c : n) {
+            result = max(result, c - '0');
+        }
+        return result;
+    }
+};
