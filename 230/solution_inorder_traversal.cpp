8aef1a9f75c3ea5443315039101467dab2916a0a
Cuda Chen <clh960524@gmail.com>
2021-06-01 20:29:13 +0800

    add C++ inorder traversal solution of problem #230

---
+++ 230/solution_inorder_traversal.cpp
@@ -0,0 +1,30 @@
+// inorder traversal
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int> arr;
+        helper(root, arr);
+        
+        return arr[k - 1];
+    }
+    
+private:
+    void helper(TreeNode *root, vector<int> &result) {
+        if(!root) return;
+        helper(root->left, result);
+        result.push_back(root->val);
+        helper(root->right, result);
+    }
+};
