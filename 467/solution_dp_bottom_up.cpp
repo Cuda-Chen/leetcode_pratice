f5f4a784559ea6e0d19c78c0572f868b54c31d31
Cuda Chen <clh960524@gmail.com>
2021-06-23 21:00:52 +0800

    add C++ DP bottom-up solution of problem #467

---
+++ 467/solution_dp_bottom_up.cpp
@@ -0,0 +1,18 @@
+// DP (bottom-up)
+class Solution {
+public:
+    int findSubstringInWraproundString(string p) {
+        vector<int> cnt(26, 0);
+        int len = 0;
+        
+        for(int i = 0; i < p.size(); i++) {
+            if(i > 0 && (p[i] == p[i - 1] + 1 || p[i - 1] - p[i] == 25))
+                len++;
+            else
+                len = 1;
+            cnt[p[i] - 'a'] = max(cnt[p[i] - 'a'], len);
+        }
+        
+        return accumulate(cnt.begin(), cnt.end(), 0);
+    }
+};
