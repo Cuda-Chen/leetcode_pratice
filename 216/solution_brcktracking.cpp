0fdc9db5a84dae8e2c6f35936ba4bc3ebf50f5bd
Cuda-Chen <clh960524@gmail.com>
2022-12-07 17:53:47 +0800

    Add backtracking C++ solution of problem #216

---
+++ 216/solution_brcktracking.cpp
@@ -0,0 +1,27 @@
+// backtracking
+
+class Solution {
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> ans;
+        vector<int> cur;
+        dfs(k, n, 1, cur, ans);
+        return ans;
+    }
+
+private:
+    void dfs(int k, int n, int s, vector<int> &cur, vector<vector<int>> &ans) {
+        if(k == 0) {
+            if(n == 0)
+                ans.push_back(cur);
+            return;
+        }
+        for(int i = s; i <= 9; i++) {
+            if(i > n)
+                break;
+            cur.push_back(i);
+            dfs(k - 1, n - i, i + 1, cur, ans);
+            cur.pop_back();
+        }
+    }
+};
